af688bcd7a7b5c3dfdf48c988dc66c6b70965e8d
knaquin <kevin.naquin@digitalglobe.com>
2017-05-26 14:13:02 -0400

    Adding the actual mirror service.

---
+++ apps/omar-git-mirror-app/grails-app/services/omar/git/mirror/app/GitService.groovy
@@ -0,0 +1,49 @@
+package omar.git.mirror.app
+
+
+import grails.transaction.Transactional
+
+
+@Transactional
+class GitService {
+
+    def grailsApplication
+
+    def mirror( json ) {
+/*
+        def repoName = json.repository?.name
+        if ( !repoName ) { return }
+
+        def repoDir = new File( "${ grailsApplication.config.mirrorDir }/${ repoName }" )
+
+        // if the repo directory doesn't exists
+        if ( !repoDir.exists() ) {
+            // create it
+            repoDir.mkdir()
+
+            // clone the repo
+            def cloneCommand = "git clone --mirror ${ repository.clone_url }"
+            def cloneProcess = cloneCommand.execute( null, repoDir )
+            cloneProcess.waitFor()
+
+            // change the remote push url for the origin
+            def setUrlCommand = "git set-url --push origin ${ mirrorRepoUrl }/${ repoName }.git"
+            def setUrlProcess = setUrlCommand.execute( null, repoDir )
+            setUrlProcess.waitFor()
+        }
+
+        // fetch updates
+        def fetchCommand = "git fetch -p origin"
+        def fetchProcess = fetchCommand.execute( null, repoDir)
+        fetchCommand.waitFor()
+
+        // push updates
+        def pushCommand = "git push --mirror"
+        def pushProcess = pushCommand.execute(
+        pushProcess.waitFor())
+
+*/
+        return
+
+    }
+}
