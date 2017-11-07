30923b74ddeac8ad242c359c121dfbda6f3545d6
knaquin <kevin.naquin@digitalglobe.com>
2017-11-07 10:28:20 -0500

    Changing URL mapping.

--- apps/omar-git-mirror-app/grails-app/controllers/omar/git/mirror/app/UrlMappings.groovy
+++ apps/omar-git-mirror-app/grails-app/controllers/omar/git/mirror/app/UrlMappings.groovy
@@ -9,7 +9,10 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        "/" {
+        	action = "index"
+			controller = "git"
+        }
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
